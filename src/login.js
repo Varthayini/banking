e4ea51f850286ba31e630c735a54dfe0a632fbaa
Varthayini <19euec170@skcet.ac.in>
2022-05-10 14:31:50 +0530

    banking

---
+++ src/login.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './login.css'
+function App () {
+  return (
+    <>
+    
+    <div className="App">
+      Welcome to Crystal Delta Banking
+      <h1> Login to your account</h1>
+      <h3> Customer ID</h3>
+      <input type="text" name="Username" placeholder="Enter your email_id" size="50"></input>
+      <h3>Password</h3>
+      <input type="text" name="password" placeholder="Enter your password" size="50"></input>
+      <br>
+      </br>
+      <br>
+      </br>
+      <button  type="submit" value="submit" size="20"><span submit="align-middle">SUBMIT</span></button>
+      <br/>
+      <br/>
+      <br/>
+      
+      
+    
+
+    </div>
+ </>
+  );
+}
+export default App;
